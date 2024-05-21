8b68c322ab06018f856fc5f7703e968a95abd7f9
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-05-21 16:15:11 +0300

    latest blogs fixed, add loading, title style fixed

--- src/pages/LatestBlogs.jsx
+++ src/pages/LatestBlogs.jsx
@@ -1,81 +1,125 @@
-import { Box, CardMedia, Grid, Paper, Typography } from '@mui/material'
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { Box, CardMedia, Grid, Paper, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import useBlogRequest from "../hooks/useBlogRequest";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const LatestBlogs = () => {
-    const { blogs } = useSelector(state => state.blog)
-    const { getRequest } = useBlogRequest()
-    const navigate = useNavigate()
+  const { blogs, loading } = useSelector((state) => state.blog);
+  const { getRequest } = useBlogRequest();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getRequest("blogs")
-    }, [])
+  useEffect(() => {
+    getRequest("blogs");
+  }, []);
 
-    const latestBlogs = blogs.slice(-8);
+  const latestBlogs = blogs.slice(-8);
 
-    return (
-        <Box display="flex" flexDirection="column" alignItems="center">
-            <Typography pt={5} textAlign={'center'} variant="h6" color={"#723C45"} textTransform={'uppercase'} fontWeight={"bold"}>Latest Blogs</Typography>
-            <Box py={5} px={10}>
-                <Grid container spacing={2}>
-                    {latestBlogs.map((blog, index) => (
-                        <Grid key={index} item xs={12} md={index === 0 ? 9 : index < 4 ? 4 : 6} sx={{margin:"auto"}}>
-                            <Paper
-                                sx={{
-                                    cursor: "pointer",
-                                    height: "100%",
-                                    position: "relative",
-                                    ":hover .title": {
-                                        bottom: "40px",
-                                        width: "100%",
-                                        backgroundColor: "rgba(0, 0, 0, 0.8)",
-                                        fontSize:{xs:"8px",md:"1rem"},
-                                    },
-                                }}
-                                onClick={()=>navigate(`/blogdetail/${blog._id}`)}
-                            >
-                                <CardMedia
-                                    component="img"
-                                    image={blog.image}
-                                    alt={blog.title}
-                                    sx={{
-                                        width: "100%",
-                                        height: "100%",
-                                        objectFit: "cover",
-                                        opacity: 1,
-                                        ":hover": { opacity: 0.6 },
-                                        transition: "opacity 1s ease",
-                                    }}
-                                />
-                                <Typography
-                                    className="title"
-                                    sx={{
-                                        position: "absolute",
-                                        bottom: "10px",
-                                        left: "50%",
-                                        width: "80%",
-                                        overflow:"hidden",
-                                        textAlign: "center",
-                                        transform: "translateX(-50%)",
-                                        backgroundColor: "rgba(0, 0, 0, 0.5)",
-                                        color: "white",
-                                        padding: "8px",
-                                        whiteSpace: "nowrap",
-                                        transition: "all 1s ease-in",
-                                        fontSize:{xs:"6px", md:"12px"},
-                                    }}
-                                >
-                                    {blog.title}
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-        </Box>
-    )
-}
+  return loading ? (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress sx={{ bgcolor: "#5B92A8" }} />
+    </Box>
+  ) : (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      minHeight={"79vh"}
+      py={2}
+    >
+      <Typography
+        textAlign={"center"}
+        variant="h6"
+        color={"brown"}
+        textTransform={"uppercase"}
+        fontWeight={"bold"}
+        sx={{
+          bgcolor: "#FFE2A880",
+          borderTopRightRadius: "10px",
+          borderBottomLeftRadius: "10px",
+          width: { xs: "75vw", md: "50vw" },
+          py: 1,
+          letterSpacing: ".3rem",
+        }}
+      >
+        Latest Blogs
+      </Typography>
+      <Box py={5} sx={{ px: { xs: 5, md: 10 } }}>
+        <Grid container spacing={5}>
+          {latestBlogs.map((blog, index) => (
+            <Grid
+              key={index}
+              item
+              xs={12}
+              sm={index === 0 ? 9 : index < 4 ? 4 : 6}
+              md={index === 0 ? 9 : index < 4 ? 4 : 6}
+              sx={{
+                margin: "auto",
+                height:
+                  index === 0
+                    ? { xs: 250, sm: 350, md: 500 }
+                    : index < 4
+                    ? { xs: 250, md: 300 }
+                    : { xs: 250, md: 450 },
+              }}
+            >
+              <Paper
+                sx={{
+                  boxShadow: "5px 3px 20px gray",
+                  cursor: "pointer",
+                  height: "100%",
+                  position: "relative",
+                  ":hover .title": {
+                    bottom: "40px",
+                    width: "100%",
+                    backgroundColor: "#5B92A8",
+                    fontSize: { xs: "8px", md: "1rem" },
+                  },
+                }}
+                onClick={() => navigate(`/blogdetail/${blog._id}`)}
+              >
+                <CardMedia
+                  component="img"
+                  image={blog.image}
+                  alt={blog.title}
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    opacity: 1,
+                    ":hover": { opacity: 0.6 },
+                    transition: "opacity 1s ease",
+                  }}
+                />
+                <Typography
+                  className="title"
+                  sx={{
+                    position: "absolute",
+                    bottom: "10px",
+                    left: "50%",
+                    width: "80%",
+                    overflow: "hidden",
+                    textAlign: "center",
+                    transform: "translateX(-50%)",
+                    backgroundColor: "#5B92A870",
+                    color: "white",
+                    padding: "8px",
+                    whiteSpace: "nowrap",
+                    transition: "all 1s ease-in",
+                    fontSize: { xs: "6px", md: "12px" },
+                    whiteSpace: "normal",
+                  }}
+                >
+                  {blog.title}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
 
 export default LatestBlogs;
--- src/pages/PopularBlogs.jsx
+++ src/pages/PopularBlogs.jsx
@@ -1,12 +1,36 @@
 import { Box, Typography } from "@mui/material";
 import MostCommentedBlogs from "../components/MostCommentedBlogs";
+
 const PopularBlogs = () => {
   return (
-    <Box display={"flex"}  flexDirection={"column"} alignItems={"center"} py={2}>
-      <Typography pt={5} textAlign={'center'} variant="h6" color={"#723C45"} textTransform={'uppercase'} fontWeight={"bold"}>Popular Blogs</Typography>
-      <MostCommentedBlogs/>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      py={2}
+      minHeight={"79vh"}
+    >
+      <Typography
+        pt={5}
+        textAlign={"center"}
+        variant="h6"
+        color={"brown"}
+        textTransform={"uppercase"}
+        fontWeight={"bold"}
+        sx={{
+          bgcolor: "#FFE2A880",
+          borderTopRightRadius: "10px",
+          borderBottomLeftRadius: "10px",
+          width: { xs: "75vw", md: "50vw" },
+          py: 1,
+          letterSpacing: ".3rem",
+        }}
+      >
+        Popular Blogs
+      </Typography>
+      <MostCommentedBlogs />
     </Box>
-  )
-}
+  );
+};
 
-export default PopularBlogs
+export default PopularBlogs;
